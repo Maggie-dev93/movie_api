1d647671ddc11e4027c5800f905dd4beb3a6ae88
Maggie-dev93 <maggieneubiglong@gmail.com>
2024-04-24 12:27:21 +0000

    change

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express();
 const { check, validationResult } = require('express-validator');
 
 
-//mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.use(bodyParser.json());
