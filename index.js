16e6803f19efc2b2f5342df87a3de04eb1a26d6d
Maggie-dev93 <maggieneubiglong@gmail.com>
2024-04-24 12:23:09 +0000

    connection uri

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ const Users = Models.User;
 // Initialize your express application
 const app = express();
 
+
 const { check, validationResult } = require('express-validator');
 
+
+//mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
