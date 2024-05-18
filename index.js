09780f545bce84816a202e3813062699fc9ee39c
Maggie-dev93 <maggieneubiglong@gmail.com>
2024-05-18 15:09:08 +0000

    added login and doc

--- index.js
+++ index.js
@@ -4,45 +4,38 @@ const morgan = require("morgan");
 const mongoose = require('mongoose');
 const Models = require('./models.js');
 const { check, validationResult } = require("express-validator");
-const cors = require("cors");
 
 const Movies = Models.Movie;
 const Users = Models.User;
 
-// Initialize your express application
-const app = express();
 
-let allowedOrigins = [
-  "http://localhost:8080",
-  "http://localhost:1234",
-  "https://movies-flixmcn-ed96d6a64be1.herokuapp.com",
-  "http://localhost:4200",
-];
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-
-// Use the CORS middleware with the configured options
-app.use(cors(corsOptions));
 
 console.log('starting mongo connection');
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 //mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 console.log('connected to mongo');
 
+const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('common')); // Uncomment for HTTP request logging
 app.use(express.static('public'));
 
-let auth = require('./auth')(app);
+const cors = require('cors');
+const allowedOrigins = ['http://localhost:8080', 'https://movies-flixmcn-ed96d6a64be1.herokuapp.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('The CORS policy for this application does not allow access from the specified origin.'));
+    }
+  }
+}));
+
+const auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
@@ -60,6 +53,49 @@ app.get("/", (req, res) => {
   Email: String,
   BirthDate: Date
 }*/
+
+
+/* POST login. */
+/* POST endpoint to add a new user */
+app.post('/users', 
+  // Validation logic here for request
+  [
+    check('Username', 'Username is required').isLength({ min: 5 }),
+    check('Username', 'Username contains non-alphanumeric characters not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], async (req, res) => {
+    // Check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    // Hash the password
+    let hashedPassword = Users.hashPassword(req.body.Password);
+
+    try {
+      // Check if the username already exists
+      let existingUser = await Users.findOne({ Username: req.body.Username });
+      if (existingUser) {
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        // Create a new user with the hashed password
+        let newUser = await Users.create({
+          Username: req.body.Username,
+          Password: hashedPassword,
+          Email: req.body.Email,
+          BirthDate: req.body.BirthDate
+        });
+        return res.status(201).json(newUser);
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send('Error: ' + error);
+    }
+  }
+);
 
 app.post('/users', 
   // Validation logic here for request
