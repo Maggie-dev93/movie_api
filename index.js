064ccea665965c33becf4ab1e45162e3103c175d
Maggie-dev93 <139060168+Maggie-dev93@users.noreply.github.com>
2024-05-21 22:12:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const Users = Models.User;
 
 
 console.log('starting mongo connection');
-//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 console.log('connected to mongo');
 
 
